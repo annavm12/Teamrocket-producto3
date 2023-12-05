bd4a21c95b0b3597bbfeef12ed2470753b2f2ab4
dacaro12 <dav.cat.roc@gmail.com>
2023-12-05 19:07:44 +0100

    primeros pasos

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+/*import firebase from './utils/Firebase';
+import "firebase/auth";
+*/
+const Home = () => {
+
+    return (
+        <View>
+            <Text>Buenas tardes patata</Text>
+        </View>
+    )
+}
+
+export default Home;
---
+++ app/utils/Firebase.js
@@ -0,0 +1,15 @@
+/*import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCXg6Dx-r--AYXDHgkVt07-UKK56AIOTEk",
+    authDomain: "team-rocket-prod2.firebaseapp.com",
+    databaseURL: "https://team-rocket-prod2-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "team-rocket-prod2",
+    storageBucket: "team-rocket-prod2.appspot.com",
+    messagingSenderId: "977220676190",
+    appId: "1:977220676190:web:dc63c4123cb200809f22cf",
+    measurementId: "G-6DE7MDL1GF"
+};
+
+export default firebase.initializeApp(firebaseConfig);
+*/
