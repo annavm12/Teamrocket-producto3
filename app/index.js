80978950e82c0eb200103fb6894c3a5c97519318
dacaro12 <dav.cat.roc@gmail.com>
2023-12-05 19:57:39 +0100

    updatas

--- app/index.js
+++ app/index.js
@@ -1,13 +1,52 @@
-import { View, Text } from 'react-native';
-/*import firebase from './utils/Firebase';
-import "firebase/auth";
-*/
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+
+
 const Home = () => {
+    const router = useRouter();
 
     return (
-        <View>
-            <Text>Buenas tardes patata</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.
+        lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""
+                }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}
+                >
+                    <Welcome>
+
+                    </Welcome>
+
+                    <Popularjobs>
+
+                    </Popularjobs>
+
+                    <Nearbyjobs>
+                        
+                    </Nearbyjobs>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { TouchableOpacity, Image } from 'react-native'
 
 import styles from './screenheader.style'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const ScreenHeaderBtn = () => {
   return (
-    <View>
-      <Text>ScreenHeaderBtn</Text>
-    </View>
+    <TouchableOpacity>
+      <Image
+        source={iconUrl}
+      />
+    </TouchableOpacity>
   )
 }
 
